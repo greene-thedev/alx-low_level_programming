6bd2235e9d806a1b0bcbfa846ff3abb4a37d5189
Jeremiah <jerrygreene010@gmail.com>
2023-08-12 09:10:09 -0700

    betty syntax implemented

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point
  *
@@ -9,6 +10,7 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -19,7 +21,7 @@ int main(void)
         printf("%d is zero\n", n);
 } else if (n < 0)
 {
-        printf("%d is negative\n", n);
+	printf("%d is negative\n", n);
 }
 	return (0);
 }
