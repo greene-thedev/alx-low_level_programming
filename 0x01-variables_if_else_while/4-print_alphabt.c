baf5a738656f30a2d26a596aa5ce4c324c1d83dc
Jeremiah <jerrygreene010@gmail.com>
2023-08-13 13:11:05 -0700

    written and compiled task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char letter;
+
+	srand(time(0));
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+	if (letter != 'q' && letter != 'e')
+	{
+		putchar(letter);
+	}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
