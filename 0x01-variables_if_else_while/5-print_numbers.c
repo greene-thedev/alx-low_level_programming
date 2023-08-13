040b1523bacc8b752701c3c02839a988fe8241a6
Jeremiah <jerrygreene010@gmail.com>
2023-08-13 13:14:47 -0700

    written and compiled task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	
+       	int digit;
+
+	for (digit = 0; digit <= 9; digit++)
+	{
+		putchar(digit + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
