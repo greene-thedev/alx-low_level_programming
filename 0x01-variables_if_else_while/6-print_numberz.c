00c5e8a5aa8080b37c46a25d2049908245c5e155
Jeremiah <jerrygreene010@gmail.com>
2023-08-14 18:41:29 -0700

    updated putchear count

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -9,16 +9,15 @@
  */
 int main(void)
 {
-	putchar(48);
-	putchar(49);
-	putchar(50);
-	putchar(51);
-	putchar(52);
-	putchar(53);
-	putchar(54);
-	putchar(55);
-	putchar(56);
-	putchar(57);
+	int i;
+
+	srand(time(0));
+
+	for (i = 0; i < 10; i++)
+	{
+	putchar('0' + i);
+	}
+
 	putchar('\n');
 
 	return (0);
